a36e389c99f01985667981e2d7a929648f51005f
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2026-05-12 16:29:25 -0700

    Revert "Improve deployment pipeline"
    This reverts commit d15d6782fd2e9e5292ae3d8aecda2a82352f361d.

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,5 @@
 @ECHO OFF
-ECHO Building React app...
-call npm run build
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO Build failed! Aborting deploy.
-    PAUSE
-    EXIT /B 1
-)
-ECHO Deploying to production...
-netlify deploy --prod --dir build
+ECHO Deploying...
+netlify deploy
 ECHO Complete
 PAUSE
